3809271971e6511a95c45bd68b5baac396fcd09f
AH228589 <AH228589@falmouth.ac.uk>
2021-09-16 20:33:39 +0100

    - Created a button prefab with all features for next iterations (on click) and imported it into the scene for the elevator.

--- Assets/Scripts/ButtonInteract.cs
+++ Assets/Scripts/ButtonInteract.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ButtonInteract : MonoBehaviour, IInteractable
 {
+    private Vector3 fix = new Vector3(1, 1, 1);
+    private void Update()
+    {
+        //Hopefully a short term cheat to make the buttons visible and stop them from scaling down to (0,0,0)
+        this.transform.localScale = fix;
+    }
+
     //This id represents the position in the 'ElevatorButtons._instance.buttons' list
     private int id;
 
@@ -15,6 +22,7 @@ public class ButtonInteract : MonoBehaviour, IInteractable
 
     public void Use()
     {
+        Debug.Log("Elevator button: " + id + " has been pressed.");
         //Run any events tied to the button
         if (ElevatorButtons._instance.buttons[id].OnButtonPress != null)
             ElevatorButtons._instance.buttons[id].OnButtonPress.Invoke();

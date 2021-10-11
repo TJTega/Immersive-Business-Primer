69e1f4dc33810ba124ea25ad6eae509cc515e242
Bogomil Petrov <BP218208@falmouth.ac.uk>
2021-10-11 22:45:57 +0100

    Starting work on Tutorial. New scripts and Audio logic.
    Added new Tutorial kogic.
    Reshaped tutorial stage.
    New Audio Logic.

--- Assets/Tutorial/Tutorial.cs
+++ Assets/Tutorial/Tutorial.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
+    public AudioSource tutorialAudio;
+    public Text subtitleText;
+    public string subtitleLine;
+    private AudioSource[] allAudioSources;
+    private bool playAvailable= false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        subtitleText.text = null;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && playAvailable == false)
+        {
+            Invoke("topAllAudio", 0);
+            playAvailable = true;
+        }
+        if (other.tag == "Player" && playAvailable == true)
+        {
+            subtitleText.text = subtitleLine;
+            tutorialAudio.Play();
+        }
+    }
+    
+   
+  
+ void Awake()
+    {
+        allAudioSources = Object.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+    }
+
+    void topAllAudio()
+    {
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+            Debug.Log("Stopping Music....");
+        }
     }
 }

a5988c094fad8b0b3016676a26c53782f125a2b9
Bogomil Petrov <BP218208@falmouth.ac.uk>
2021-10-12 12:39:25 +0100

    Final Tut before dem

--- Assets/TutorialTeleport.cs
+++ Assets/TutorialTeleport.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class TutorialTeleport : MonoBehaviour
 {
+    public Transform teleportLocation;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void OnTriggerEnter(collider other)
-    { 
-        if ()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RightPointerCollider")
+            GameObject.FindGameObjectWithTag("XRRig").transform.position = teleportLocation.position;
     }
-    private void ontr
+
 }

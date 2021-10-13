fc82b2c9497e0980d5046e0a0206d67a463e5c9d
ah228589 <ah228589@falmouth.ac.uk>
2021-10-13 12:01:07 +0100

    Changes to the scene regarding teleport locations, small changes to script to reduce repeating code and depricated calls

--- Assets/TutorialTeleport.cs
+++ Assets/TutorialTeleport.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class TutorialTeleport : MonoBehaviour
@@ -11,48 +9,38 @@ public class TutorialTeleport : MonoBehaviour
     public GameObject floor;
     public int lobbyFloorSwitch;
     public Transform teleportLocation;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     private void OnTriggerEnter(Collider other)
     {
-        switch(lobbyFloorSwitch)
-        { case 1:
-                if (other.tag == "RightPointerCollider")
-                {
+        //Only go inside and teleport player if the collider that entered is "RightPointerCollider"
+        if (other.tag == "RightPointerCollider")
+        {
+            switch (lobbyFloorSwitch)
+            {
+                case 1:
+
                     Invoke("TeleportPlayer", 0);
-                    lobby.active = true;
-                    floor.active = false;
-                }
-                break;
-            case 2:
-                if (other.tag == "RightPointerCollider")
-                {
+                    lobby.SetActive(true);
+                    floor.SetActive(false);
+                    break;
+                case 2:
+
                     Invoke("TeleportPlayer", 0);
-                    lobby.active = false;
-                    floor.active = true;
-                }
-                break;
+                    lobby.SetActive(false);
+                    floor.SetActive(true);
+                    break;
+            }
         }
-       
     }
-    
+
+    //Teleporting the player and xrRig instead of setting the transform.position
+    //This is done to prevent players going out of bounds when exploring the edges
     public void TeleportPlayer()
     {
         XRRig xrRig = GameObject.FindGameObjectWithTag("XRRig").GetComponent(typeof(XRRig)) as XRRig;
         if (xrRig != null)
         {
-
             xrRig.MatchRigUpCameraForward(teleportLocation.rotation * Vector3.up, teleportLocation.rotation * Vector3.forward);
-
 
             var heightAdjustment = xrRig.rig.transform.up * xrRig.cameraInRigSpaceHeight;
 

8a528e99ba35c9c40281a362b6457fe87cc1ec02
Tega Amrore <ta243616@falmouth.ac.uk>
2021-10-14 14:12:19 +0100

    Added scene loading scripts

---
+++ Assets/Scripts/Room Loading/PlayerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public static Vector3 playerPos;
+    public static int loadNumber = 0;
+
+    private void Update()
+    {
+        playerPos = transform.position;
+    }
+}

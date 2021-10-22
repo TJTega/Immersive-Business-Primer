dc4a24245afb1c9dfe5fc7f80916e43735431a2d
ah228589 <ah228589@falmouth.ac.uk>
2021-10-22 14:49:43 +0100

    Grabbing room settings is now done, linking them to rooms is partial

--- Assets/Scripts/File Crawling/GrabScenes.cs
+++ Assets/Scripts/File Crawling/GrabScenes.cs
@@ -1,27 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using System.Linq;
+using UnityEngine.SceneManagement;
 
-public class GrabSceneNames : MonoBehaviour
+public class GrabScenes : MonoBehaviour
 {
+    //ROOM
+    Room current;
+
     //This gets the folder structure to /Assets/
     private string m_Path;
 
     //The path for the directory to crawl
     public string defaultPath = "/Scenes/Experiences";
 
+    //List of discovered rooms and list of discovered settings
     public List<string> roomList;
+    public List<string> settingsList;
 
-    private TextAsset jsonFileContents;
+    //Dictionary key = room name, value back = settings
+    public Dictionary<string, string> roomSettings = new Dictionary<string, string>();
 
-    // Start is called before the first frame update
     void Start()
     {
+        current = ScriptableObject.CreateInstance("Room") as Room;
+
         m_Path = Application.dataPath;
 
         DirSearch(m_Path + defaultPath);
+
+        LinkRoomsToSettings();
 
     }
 
@@ -46,9 +54,9 @@ public class GrabSceneNames : MonoBehaviour
                             {
                                 PopulateRoomList(fileName);
                             }
-                            else
+                            if (fileName.Contains(".json"))
                             {
-                                GrabSettings(filePath);
+                                GrabRoomSettings(filePath);
                             }
                         }
                     }
@@ -70,18 +78,31 @@ public class GrabSceneNames : MonoBehaviour
     }
 
     //This populates the list of scriptable objects attached to rooms.
-    public void GrabSettings(string path)
+    public void GrabRoomSettings(string path)
     {
-        //TODO: Grab settings using the room name and path
-        //jsonFileContents = patwh;
+        //Grabbing settings from path
+        using (StreamReader stream = new StreamReader(path))
+        {
+            string jsonFile = stream.ReadToEnd();
+            JsonUtility.FromJsonOverwrite(jsonFile, current);
+        }
 
-        var fileName = new DirectoryInfo(path).Name;
-        var yourFile = path.FirstOrDefault();
-        print(yourFile);
-        //print(fileName);
+        string json = JsonUtility.ToJson(current, true);
+        settingsList.Add(json);
+    }
 
-        //Room current = JsonUtility.FromJson<Room>(fileName.text);
-       // string json = JsonUtility.ToJson(current);
-        //Debug.Log(json);
+    //This links each room to their Room_Settings.json
+    public void LinkRoomsToSettings()
+    {
+        for (int i = 0; i < settingsList.Count; i++)
+        {
+            string jsonData = settingsList[i];
+            string room = roomList[i];
+
+            if (!roomSettings.ContainsKey(room))
+            {
+                roomSettings.Add(room, jsonData);
+            }
+        }
     }
 }

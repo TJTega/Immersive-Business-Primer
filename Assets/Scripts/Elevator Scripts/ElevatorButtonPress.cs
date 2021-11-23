931b8f3456275ac67c7b58e1cf2e62fa014a82c7
Wankenstien <57715011+Wankenstien@users.noreply.github.com>
2021-11-23 16:50:48 +0000

    Changed the button to have emmisive shader toggle script

---
+++ Assets/Scripts/Elevator Scripts/ElevatorButtonPress.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButtonPress : TouchPress
+{
+    public MeshRenderer renderer;
+    public Material activeMat;
+    public Material inactiveMat;
+
+    
+}
--- Assets/Scripts/Elevator Scripts/TouchPress.cs
+++ Assets/Scripts/Elevator Scripts/TouchPress.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TouchPress: MonoBehaviour
+public class TouchPress : MonoBehaviour
 {
+    public MeshRenderer meshRenderer;
+    public Material activeMat;
+    public Material inactiveMat;
 
     public UnityEvent pressed;
     public string targetTag;
@@ -11,13 +14,33 @@ public class TouchPress: MonoBehaviour
     void ButtonPress()
     {
         pressed.Invoke();
+        ButtonActive();
     }
 
-    private void OnTriggerEnter(Collider other)
+    protected void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(targetTag))
         {
             ButtonPress();
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            ButtonInactive();
+        }
+    }
+
+    public void ButtonActive()
+    {
+        meshRenderer.materials[1] = activeMat;
+    }
+
+    public void ButtonInactive()
+    {
+        meshRenderer.materials[1] = inactiveMat;
+    }
+
 }

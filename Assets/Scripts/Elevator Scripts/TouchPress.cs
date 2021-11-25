476d39c2c5fa0532397a529a1221c683721f27cd
Tega Amrore <ta243616@falmouth.ac.uk>
2021-11-25 11:51:02 +0000

    Created subclass to handle elevator buttons press

--- Assets/Scripts/Elevator Scripts/TouchPress.cs
+++ Assets/Scripts/Elevator Scripts/TouchPress.cs
@@ -4,47 +4,20 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TouchPress : MonoBehaviour
 {
-    public MeshRenderer meshRenderer;
-    public Material activeMat;
-    public Material inactiveMat;
-
     public UnityEvent pressed;
     public string targetTag;
 
     [ContextMenu("Press")]
-    void ButtonPress()
+    protected virtual void ButtonPress()
     {
-        ButtonActive();
         pressed.Invoke();
     }
 
-    protected void OnTriggerEnter(Collider other)
+    protected virtual void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(targetTag))
         {
             ButtonPress();
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag(targetTag))
-        {
-            ButtonInactive();
-        }
-    }
-
-    public void ButtonActive()
-    {
-        var mats = meshRenderer.materials;
-        mats[1] = activeMat;
-        meshRenderer.materials = mats;
-    }
-
-    public void ButtonInactive()
-    {
-        var mats = meshRenderer.materials;
-        mats[1] = inactiveMat;
-        meshRenderer.materials = mats;
     }
 }

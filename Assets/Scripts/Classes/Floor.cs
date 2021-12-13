402d73430e1eb76beb33cd3508c353e26a744dec
Tega Amrore <ta243616@falmouth.ac.uk>
2021-12-13 14:52:20 +0000

    Cleaned up floor and room constructors

--- Assets/Scripts/Classes/Floor.cs
+++ Assets/Scripts/Classes/Floor.cs
@@ -20,17 +20,21 @@ public class Rooms
 [CreateAssetMenu(fileName = "Floor", menuName = "Scriptable Objects/Floor", order = 1)]
 public class Floor : ScriptableObject
 {
+    [Header("Meta")]
     ///<summary>This is the displayed name of each floor</summary>
     public string floorName;
     ///<summary>This is a categorisation tag</summary>
     public string tag;
 
+    [Header("Materials")]
     ///<summary>General material for the ceiling of each floor</summary>
     public Material ceilingMat;
     ///<summary>General material for the walls of each floor</summary>
     public Material wallMat;
     ///<summary>General material for the floor of each floor</summary>
     public Material floorMat;
+
+    [Header("Meshes")]
     ///<summary>This holds an asset for that would be displayed as skirting</summary>
     public GameObject skirting;
     ///<summary>This holds an asset for that would be displayed as overheads</summary>
@@ -38,9 +42,15 @@ public class Floor : ScriptableObject
     ///<summary>This holds the volume settings for each floor</sumskimary>
     public VolumeProfile volumeProfile;
 
-    ///<summary>This is a scriptable object holding general assets for floors</summary>
+    ///<summary>This is a gameObject holding general assets for floors</summary>
     public GameObject subsceneAssets;
+    /// <summary>This holds the prefab for the floor doors</summary>
     public GameObject doors;
+
+    ///<summary>This is ambient sound for the floor</summary>
+    public AudioClip ambience;
+
+    [Header("Rooms")]
     ///<summary>Class containing left, back and right rooms</summary>
     public Rooms rooms;
 }

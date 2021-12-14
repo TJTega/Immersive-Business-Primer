2bc8eac575b96e82648e9394f5e4c9d871b67e20
Bogomil Petrov <BP218208@falmouth.ac.uk>
2021-12-14 22:54:52 +0000

    Fixed Room Doors

---
+++ Assets/RoomDoors.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class RoomDoors : MonoBehaviour
+{
+    public DOTweenAnimation door;
+    // Start is called before the first frame update
+     void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+            {
+            door.DOPlayForward();
+        }
+    }
+     void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            door.DOPlayBackwards();
+        }
+    }
+}
